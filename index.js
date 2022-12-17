f2f5a388b7d9a891946db7b436f5fc17c4a11989
Harry Lomas <harry.lomas1@outlook.com>
2022-12-17 13:19:34 +0000

    added deck array and returns 52 card deck

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
     let values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
     let suits = ["H", "D", "C", "S"];
+    let deck = [];
     
 
 for(let suitCounter = 0; suitCounter <4; suitCounter++) {
     for (let valueCounter = 0; valueCounter < 13; valueCounter++){
-        console.log(values[valueCounter] + suits[suitCounter]); //added nexted function to combine 2 functions and return a 52 card deck
+        deck.push(values[valueCounter] + suits[suitCounter]); //added nexted function to combine 2 functions and return a 52 card deck
     } 
+
 }
 
+console.log(deck);
