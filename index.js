1ddd1e20a994a997b13201a6a49a5a4dfba9baf5
Harry Lomas <harry.lomas1@outlook.com>
2023-01-02 12:56:44 +0000

    dealer hand and player hand added

--- index.js
+++ index.js
@@ -2,30 +2,40 @@ function createDeck() {
 
     let values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
     let suits = ["H", "D", "C", "S"];
-    let deck = [];
+    let deck = []; // create deck from 2 array
     
 
 for(let suitCounter = 0; suitCounter <4; suitCounter++) {
     for (let valueCounter = 0; valueCounter < 13; valueCounter++){
-        deck.push(values[valueCounter] + suits[suitCounter]); //added nexted function to combine 2 functions and return a 52 card deck
+        deck.push(values[valueCounter] + suits[suitCounter]); //added nested function to combine 2 functions and return a 52 card deck
     } 
 
  } 
   return deck;
 
 }
-function shuffleDeck(deck) {
+function shuffleDeck(deck) { //function to shuffle the deck
     for(let i = 0; i <52; i++) {
-    let tempCard = deck[i];
-    let randomIndex = Math.floor(Math.random() * 52); 
-    deck[i] = deck[randomIndex]
-    deck[randomIndex] = tempCard;
+        let tempCard = deck[i];
+        let randomIndex = Math.floor(Math.random() * 52); 
+        deck[i] = deck[randomIndex]
+        deck[randomIndex] = tempCard;
   }
 }
 
-let testDeck = createDeck();
-shuffleDeck(testDeck);
-console.log(testDeck);
+let deck = createDeck();
+shuffleDeck(deck);
+console.log(deck);
+
+let playerCards = [deck.pop(), deck.pop()];
+
+console.log (playerCards);
+
+let dealerCards = [deck.pop(), deck.pop()];
+
+console.log(dealerCards);
+
+
 
 
 
