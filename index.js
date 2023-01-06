d9bda2313783922fff8f1c9a3897ee0de8c25f4d
Harry Lomas <harry.lomas1@outlook.com>
2023-01-06 16:04:46 +0000

    updated styling and added startGame function

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 let firstCard = 10
-let secondCard = 1
-let sum = firstCard + secondCard + 4
+let secondCard = 11
+let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true 
+let message = ""
 
-if (sum <=20) {
-    console.log("Do you want to hit?")
-}else if (sum === 21)  {
-    console.log  ("BlackJack")
-hasBlackJack = true
-} else {
-    console.log("Bust")
+function startGame() {
+    if (sum <=20) {
+    message = "Do you want to hit?"
+    } else if (sum === 21)  {
+    message = "BlackJack"
+    hasBlackJack = true
+    } else {
+    message = "Bust"
     isAlive = false
+    }
+
+console.log(message)
+
 }
 
 
